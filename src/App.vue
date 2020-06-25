1f875e73b9f500b328345cac818e463a72fc41e2
chaichai <2646162809@qq.com>
2020-06-25 17:48:04 +0800

    添加card页面

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>hello!this is my first vue-demo on webstorm</h1>
+      <cart></cart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+    import cart from "@/components/cart";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      cart
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/cart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        this is a card
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cart"
+    }
+</script>
+
+<style scoped>
+
+</style>
